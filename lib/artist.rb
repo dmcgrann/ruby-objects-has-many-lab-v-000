b9a34de5a71ae258fefbaf6f3a34b7921c8b0f7d
David McGrann <dpmcgrann@gmail.com>
2018-04-08 16:55:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,17 +11,16 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(title)
     song = Song.new(title)
     @songs << song
     song.artist = self
-
   end
 
   def self.song_count
+    @@song_count += 1
     @@song_count
   end
 
